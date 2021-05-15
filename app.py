d60125c99629d269109475c16854a7f139793388
Renan Santos <renan3santos@gmai.com>
2021-05-15 20:12:27 -0300

    Padronizando para application factory

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import routes
+import database
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+    database.configure(app)
+    routes.configure(app)
+    return app
 
-@app.route('/')
-def index():
-    return 'Todo List'
 
