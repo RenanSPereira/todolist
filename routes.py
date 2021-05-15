4add68a466939d967af0efa878c7f7d4a37d36cc
Renan Santos <renan3santos@gmai.com>
2021-05-15 20:33:42 -0300

    Rota para adicionar tarefa/todo

--- routes.py
+++ routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, request, url_for, redirect
 from models import Todo
+from database import db
 
 def configure(app):
     
@@ -7,5 +8,12 @@ def configure(app):
     def index():
         todos = Todo.query.all()
         return render_template('index.html', todos=todos)
+
+    @app.route('/add', methods=['POST'])
+    def add():
+        todo = Todo(description=request.form['description'], finished=False)
+        db.session.add(todo)
+        db.session.commit()
+        return redirect(url_for('index'))
 
     return app    
